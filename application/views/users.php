38637be3dd964df98f155f9d4731d3d5b85d9913
dough14 <dough14@outlook.com>
2014-01-05 00:31:19 -0200

    major changes

---
+++ application/views/users.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Douglas
+ * Date: 05/01/14
+ * Time: 00:08
+ */
+
+// Load Menu
+$this->template->menu('users');
+?>
+
+<div id="content" class="container_16 clearfix">
+    <div class="grid_4">
+        <p>
+            <label>Cliente <small>nome ou CPF/CNPJ</small></label>
+            <input type="text" />
+        </p>
+    </div>
+    <div class="grid_5">
+        <p>
+            <label>Endereço de email</label>
+            <input type="text" />
+        </p>
+    </div>
+    <div class="grid_5">
+        <p>
+            <label>Tipo de processo</label>
+            <select>
+                <option>Judiciário</option>
+                <option>Previdenciário</option>
+                <option>Cívil</option>
+                <option>Trabalhista</option>
+            </select>
+        </p>
+    </div>
+    <div class="grid_2">
+        <p>
+            <label>&nbsp;</label>
+            <input type="submit" value="Search" />
+        </p>
+    </div>
+    <div class="grid_16">
+        <?php if(isset($users)) { ?>
+        <table>
+            <thead>
+            <tr>
+                <th>Nome ou CPF/CNPJ</th>
+                <th>Processo</th>
+                <th>Andamento</th>
+                <th colspan="2" width="10%">Ações</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td colspan="5" class="pagination">
+                    <span class="active curved">1</span><a href="#" class="curved">2</a><a href="#" class="curved">3</a><a href="#" class="curved">4</a> ... <a href="#" class="curved">10 million</a>
+                </td>
+            </tr>
+            </tfoot>
+            <tbody>
+            <?php foreach ($users as $user) { ?>
+                <tr id="user_<?php echo $user['id']; ?>">
+                    <td><?php echo $user['id']; ?></td>
+                    <td><?php echo $user['email']; ?></td>
+                    <td><?php echo $level_list[$user['level']]; ?></td>
+                    <td><?php echo date("j/M/Y, g:i a", strtotime($user['date_created'])); ?></td>
+                    <td>
+                        <?php echo anchor('user/edit/'.$user['id'],'Editar', 'class="edit"'); ?>
+                        <?php echo anchor('user/remove/'.$user['id'], 'Deletar', 'class="remove-user-event delete"'); ?>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+        <?php } ?>
+    </div>
+</div>
---
+++ application/views/users_add.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Douglas
+ * Date: 05/01/14
+ * Time: 00:28
+ */
+
+
+// Load Menu
+$this->template->menu('users');
+?>
+
+<div id="container">
+
+    <?php echo form_open('user/save'); ?>
+
+    <table>
+        <tr>
+            <td>
+                <?php echo form_label('Login', 'login'); ?>
+            </td>
+            <td>
+                <?php echo form_input('login', $login); ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo form_label('Password', 'password'); ?>
+            </td>
+            <td>
+                <?php if (isset($id)) { ?>
+                    <?php echo form_password('password', $password, 'id="password" disabled'); ?>
+                    <?php echo form_checkbox('reset_password', 1, false, 'id="reset_password" title="Edit Password"'); ?>
+                <?php } else { ?>
+                    <?php echo form_password('password', $password, 'id="password"'); ?>
+                    <?php echo form_hidden('reset_password', 1); ?>
+                <?php } ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo form_label('Level', 'level'); ?>
+            </td>
+            <td>
+                <?php echo form_dropdown('level', $level_list, $level); ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <?php if (isset($id)) echo form_hidden('id', $id); ?>
+                <div class="form-save-buttons">
+                    <?php echo form_submit('save', 'Save', 'class="btn-blue"'); ?>
+                    <?php echo form_button('cancel', 'Cancel', 'class="btn-blue" onClick="history.go(-1)"'); ?>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <?php echo form_close(); ?>
+
+</div>
